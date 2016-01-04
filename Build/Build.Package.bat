b0b0f7b5e2109056e8b9300ac5528fd28771555b
Soheil Rashidi <contact@soheilrashidi.com>
2016-01-04 11:15:15 +0330

    Updated to MSBuild 14.

--- Build/Build.Package.bat
+++ Build/Build.Package.bat
@@ -1,2 +1,2 @@
-"%PROGRAMFILES(X86)%\MSBuild\12.0\Bin\MSBuild.exe" Scripts\Build.Package.proj /target:BuildPackage
+"%PROGRAMFILES(X86)%\MSBuild\14.0\Bin\MSBuild.exe" Scripts\Build.Package.proj /target:BuildPackage
 @PAUSE
